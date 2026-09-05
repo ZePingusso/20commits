8ba3358a52c61f2e4e79e1c5df26be0d0d776b99
ZePingusso <lucasscruz622@gmail.com>
2026-09-04 21:55:13 -0400

    subindo alterações no js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function mostrarMensagem() {
+
     const mensagem = document.getElementById("mensagem");
 
-    mensagem.textContent = "Você clicou no botão!";
+    mensagem.textContent =
+        "🍿 Minha recomendação: escolha um filme e aproveite!";
+}
+
+
+function enviarFormulario(event) {
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+
+    const resposta = document.getElementById("resposta");
+
+    resposta.textContent =
+        "Obrigado pela mensagem, " + nome + "!";
+
 }
